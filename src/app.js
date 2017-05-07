3eb411f4683649f5239830836ed6a8b8e841afdb
jarekjaro <krzeminskijarek@gmail.com>
2017-05-07 12:05:46 +0200

    added email and password input sections

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 import React, {Component} from "react";
-import {Text, View} from "react-native";
+import {View} from "react-native";
+import firebase from "firebase";
 import {Header} from "./components/common";
+import LoginForm from "./components/LoginForm";
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp(
+            {
+                apiKey: "AIzaSyDirWmVgaQ9Tfx2TO7V_VZxoDpz9xlIOkc",
+                authDomain: "auth-95420.firebaseapp.com",
+                databaseURL: "https://auth-95420.firebaseio.com",
+                projectId: "auth-95420",
+                storageBucket: "auth-95420.appspot.com",
+                messagingSenderId: "848454790769"
+            }
+        );
+    }
 
     render() {
         return (
             <View>
-                <Header headerText="authentication"/>
-                <Text>An App!</Text>
+                <Header headerText="Authentication"/>
+                <LoginForm/>
             </View>
         );
     }
